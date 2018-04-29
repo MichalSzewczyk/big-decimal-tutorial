e6d23cf59aabfacc4dca0f6241d6387272b506ef
mszewczyk <szewczykm94@gmail.com>
2018-04-29 14:23:02 +0200

    Added main class and project configuration

---
+++ src/main/java/com/learning/bigdecimal/Main.java
@@ -0,0 +1,11 @@
+package com.learning.bigdecimal;
+
+/*
+ * This project aims to understand and show how BigDecimal works in Java.
+ * Examples of usage and comparison with double/float are in unit tests.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
